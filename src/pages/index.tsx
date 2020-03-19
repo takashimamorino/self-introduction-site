253638c886523cf7dd5a74c6dae81a8b778c2922
takashimamorino <does1026@gmail.com>
2020-03-19 23:21:42 +0900

    skillsの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,37 +19,36 @@ type Props = {
   skills: Skills
 }
 
-const Home: NextPage<Props> = ({ skills }) => {
-  console.log(skills)
-  return (
-    <>
-      <Navigation />
-      <main className={styles.mainContainer}>
-        <div className={styles.inner}>
-          <article className={styles.aboutContainer}>
-            <div className={styles.profile}>
-              <img src="/image_profile.svg" alt="プロフィール画像" />
-              <div className={styles.myName}>
-                <h2 className={styles.myNameJapanease}>高島克彦</h2>
-                <p className={styles.myNameAlphabet}>Takashima Katsuhiko</p>
-              </div>
+const Home: NextPage<Props> = ({ skills }) => (
+  <>
+    <Navigation />
+    <main className={styles.mainContainer}>
+      <div className={styles.inner}>
+        <article className={styles.aboutContainer}>
+          <div className={styles.profile}>
+            <img src="/image_profile.svg" alt="プロフィール画像" />
+            <div className={styles.myName}>
+              <h2 className={styles.myNameJapanease}>高島克彦</h2>
+              <p className={styles.myNameAlphabet}>Takashima Katsuhiko</p>
             </div>
-            <p className={styles.lead}>
-              組織もエンジニアリングできるエンジニアを目指しています。技術だけではなく、サービス作り、組織論にも関心があります。
-            </p>
-            <SNS />
-          </article>
-          <article className={styles.skillsContainer}>
-            <SectionTitle title="SKILLS" />
-            <h2 className={styles.skillsHeader}>言語・フレームワークなど</h2>
-            <h2 className={styles.skillsHeader}>クラウド・その他</h2>
-            <p className={styles.annotation}>※実業務で使用したもののみ</p>
-          </article>
-        </div>
-      </main>
-    </>
-  )
-}
+          </div>
+          <p className={styles.lead}>
+            組織もエンジニアリングできるエンジニアを目指しています。技術だけではなく、サービス作り、組織論にも関心があります。
+          </p>
+          <SNS />
+        </article>
+        <article className={styles.skillsContainer}>
+          <SectionTitle title="SKILLS" />
+          <h2 className={styles.skillsHeader}>言語・フレームワークなど</h2>
+          <p className={styles.skillsText}>{skills.languages}</p>
+          <h2 className={styles.skillsHeader}>クラウド・その他</h2>
+          <p className={styles.skillsText}>{skills.other}</p>
+          <p className={styles.annotation}>※実業務で使用したもののみ</p>
+        </article>
+      </div>
+    </main>
+  </>
+)
 
 export const getStaticProps: GetStaticProps = async () => {
   const key = {
