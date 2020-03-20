608575c64dc3c63371c92bf559a313aee531659f
takashimamorino <does1026@gmail.com>
2020-03-20 16:33:40 +0900

    metaの追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,4 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext
-} from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -14,8 +8,22 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
-        <Head></Head>
+      <Html lang="ja">
+        <Head>
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <meta charSet="utf-8" />
+          <title>Takashima Katsuhiko</title>
+          <meta name="description" content="都内でエンジニアしています。" />
+          {/* <meta property="og:url" content="" /> */}
+          <meta property="og:title" content="Takashima Katsuhiko" />
+          <meta property="og:type" content="website" />
+          <meta property="og:description" content="都内でエンジニアしています。" />
+          {/* <meta property="og:image" content="" /> */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@takashima_katsu" />
+          <meta property="og:site_name" content="Takashima Katsuhiko" />
+          {/* <link rel="icon" href="" /> */}
+        </Head>
         <body>
           <Main />
           <NextScript />
